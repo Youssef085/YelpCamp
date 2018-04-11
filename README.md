300535f9027bb3ff359dbfe8064b732472bfb298
EC2 Default User <ec2-user@ip-172-31-37-19.us-east-2.compute.internal>
2018-04-11 16:31:45 +0000

    Added the ability to submit

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 #YelpCamp
 
+
+============ V1 ============
 * Add Landing Page
 * Add Campgrounds Page that lists all Campgrounds
 * 
@@ -8,3 +10,9 @@ Each Campground Has:
     *Name
     *Image
 
+============ V2 ============
+#Creating New Campgrounds
+*Setup new campground Post route
+*Add in body-parser
+*Setup route to show form
+*Add basic unstyled form
--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+        {name: "Salmon Greek", image: "http://theshoppersweekly.com/wp-content/uploads/2018/03/camping.jpg"},
+        {name: "Granite Hill", image: "https://i1.trekearth.com/photos/99406/tentipi.jpg"},
+        {name: "Mountain Goats Rest", image: "https://onmilwaukee.com/images/articles/ca/camping/camping_fullsize_story1.jpg"}
+];
+
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name: "Salmon Greek", image: "http://theshoppersweekly.com/wp-content/uploads/2018/03/camping.jpg"},
-        {name: "Granite Hill", image: "https://i1.trekearth.com/photos/99406/tentipi.jpg"},
-        {name: "Mountain Goats Rest", image: "https://onmilwaukee.com/images/articles/ca/camping/camping_fullsize_story1.jpg"}
-    ]
+    
     
     res.render("campgrounds",{campgrounds:campgrounds});
 });
 
+
+app.post("/campgrounds", function(req, res){
+    //res.send("YOU HIT THE POST ROUTE!")
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image}
+    campgrounds.push(newCampground);
+    //redirect back to campgrounds page
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new.ejs");
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp Server Has Started!")
